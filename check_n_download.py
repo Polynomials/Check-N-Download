9b4e8f0e35d7c80534a944f6c88ae4fb3c2d9ac2
Unknown <tobiaswembrand@gmail.com>
2018-04-09 16:15:32 +0200

    Linux fix 2, removed colors, code clean-up
    1. Fixed Linux compatibility by using a forward slash instead of backslash for filepaths. This time including the .txt generated for links not availabe for download.

--- check_n_download.py
+++ check_n_download.py
@@ -8,8 +8,8 @@ import urllib.request
 
 def create_config():
     config = configparser.ConfigParser()
-    if os.path.isfile("{}/praw.ini".format(str(os.getcwd()))) is False:
-        print("\033[91m" + "praw.ini not found. Creating..." + "\033[0m")
+    if os.path.isfile(f"{str(os.getcwd())}/praw.ini") is False:
+        print("praw.ini not found. Creating...")
         file = open("praw.ini", "w+")
         config["Authentication"] = {"username": "Your reddit username",
                                     "password": "Your reddit password",
@@ -23,16 +23,15 @@ def create_config():
             print("Exiting program in: " + str(second))
             time.sleep(1)
         sys.exit(0)
-    elif os.path.isfile(str(os.getcwd()) + "\praw.ini") is True:
+    elif os.path.isfile(f"{str(os.getcwd())}/praw.ini") is True:
         config.read("praw.ini")
-        print("\033[92m" + "praw.ini found! Proceeding..." + "\033[0m")
+        print("praw.ini found! Proceeding with download...")
     get_settings()
 
 
 def get_settings():
     config = configparser.ConfigParser()
     config.read("praw.ini")
-
     get_settings.path = config["Settings"]["path"]
     get_settings.user = config["Users"]["user"]
 
@@ -53,8 +52,8 @@ def download_submissions(path, post_id, url):
         name = os.path.join(path, str(post_id) + ".gif")
         urllib.request.urlretrieve(url, name)
     else:
-        exception_url = open("{}{}-links.txt".format(get_settings.path, get_settings.user), "a+")
-        exception_url.write("{}\n".format(url))
+        exception_url = open(f"{get_settings.path}/{get_settings.user}-links.txt", "a+")
+        exception_url.write(f"{url}\n")
 
 
 def initialization(path, user):
@@ -63,44 +62,39 @@ def initialization(path, user):
         download_submissions(path, submission.id, submission.url)
         old_submission_amount += 1
         time.sleep(2)
-    print("\033[92m" + "Initialization successful!" + "\033[0m")
-    print("\033[35m" + "Submission amount: " + "\033[0m" + "\033[94m" + str(old_submission_amount) + "\033[0m")
+    print("Initialization successful!")
+    print("Submission amount: " + str(old_submission_amount))
     return old_submission_amount
 
 
 def check_submissions(path, user, old_submission_amount):
     while True:
         new_submission_amount = 0
-
         for submission in user.submissions.new(limit=None):
             new_submission_amount += 1
 
         if old_submission_amount < new_submission_amount:
-            print("\n" + "\033[92m" + time.strftime("%H:%M:%S ") + "New submission found!" + "\033[0m")
-            print("\033[35m" + "Old submission amount: " + "\033[0m" + "\033[94m" + str(old_submission_amount) + "\033[0m")
-            print("\033[35m" + "New submission amount: " + "\033[0m" + "\033[94m" + str(new_submission_amount) + "\033[0m")
+            print("\n" + time.strftime("%H:%M:%S ") + "New submission found!")
+            print("Old submission amount: " + str(old_submission_amount))
+            print("New submission amount: " + str(new_submission_amount))
             old_submission_amount = 0
             for submission in user.submissions.new(limit=None):
                 download_submissions(path, submission.id, submission.url)
                 old_submission_amount += 1
         elif old_submission_amount > new_submission_amount:
-            print("\n" + "\033[91m" + time.strftime("%H:%M:%S ") + "Submission removed!" + "\033[0m")
-            print("\033[35m" + "Old submission amount: " + "\033[0m" + "\033[94m" + str(old_submission_amount) + "\033[0m")
-            print("\033[35m" + "New submission amount: " + "\033[0m" + "\033[94m" + str(new_submission_amount) + "\033[0m")
+            print("\n" + time.strftime("%H:%M:%S ") + "Submission removed!")
+            print("Old submission amount: " + str(old_submission_amount))
+            print("New submission amount: " + str(new_submission_amount))
             old_submission_amount = 0
-
         time.sleep(10)
 
 
 def main():
     create_config()
-
     reddit = authenticate()
     path = get_settings.path
     user = reddit.redditor(get_settings.user)
-
     old_submission_amount = initialization(path, user)
-
     check_submissions(path, user, old_submission_amount)
 
 
